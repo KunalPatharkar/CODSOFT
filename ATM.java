cb8949e25de1284c38afdc9a9515a0c6e32c63e1
KunalPatharkar <125534620+KunalPatharkar@users.noreply.github.com>
2023-07-24 13:02:32 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -1,34 +1,24 @@
 import java.util.*;
 public class ATM {
-    static double balance = 1000.0;
     static Scanner scanner = new Scanner(System.in);
+    static BankAccount account = new BankAccount(1000.0);
     public static void main(String[] args) {
         while (true) {
-            System.out.println("\n1. Check Balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit");
-            System.out.print("\nEnter your choice: ");
+            System.out.println("\n1.Check Balance \n2.Deposit \n3.Withdraw \n4.Exit \nEnter your choice: ");
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Current balance: " + balance);
+                    checkBalance();
                     break;
                 case 2:
                     System.out.print("Enter deposit amount: ");
                     double depositAmount = scanner.nextDouble();
-                    balance += depositAmount;
-                    System.out.println("Successfully deposited " + depositAmount);
+                    deposit(depositAmount);
                     break;
                 case 3:
                     System.out.print("Enter withdraw amount: ");
                     double withdrawAmount = scanner.nextDouble();
-                    if (withdrawAmount <= balance) {
-                        balance -= withdrawAmount;
-                        System.out.println("Successfully withdrew " + withdrawAmount);
-                    } else {
-                        System.out.println("Insufficient balance.");
-                    }
+                    withdraw(withdrawAmount);
                     break;
                 case 4:
                     System.out.println("Exiting...");
@@ -36,6 +26,31 @@ public class ATM {
                 default:
                     System.out.println("Invalid choice. Please choose again.");
             }
+        }  }
+    static void checkBalance() {
+        System.out.println("Current balance: " + account.getBalance());
+    }
+    static void deposit(double amount) {
+        account.setBalance(account.getBalance() + amount);
+        System.out.println("Successfully deposited " + amount);
+    }
+    static void withdraw(double amount) {
+        if (amount <= account.getBalance()) {
+            account.setBalance(account.getBalance() - amount);
+            System.out.println("Successfully withdrew " + amount);
+        } else {
+            System.out.println("Insufficient balance.");
         }
+    }}
+class BankAccount {
+    private double balance;
+    BankAccount(double balance) {
+        this.balance = balance;
+    }
+    double getBalance() {
+        return balance;
+    }
+    void setBalance(double balance) {
+        this.balance = balance;
     }
 }
